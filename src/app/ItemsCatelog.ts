b9210c6f13dee835cc28528e7c00d44d0e84fb9c
Hope <hopeemccarthy@gmail.com>
2026-04-16 15:34:27 -0400

    updated item catelog

--- src/app/ItemsCatelog.ts
+++ src/app/ItemsCatelog.ts
@@ -66,4 +66,68 @@ export const ItemsList: ItemListingInfo[] = [
       description: '',
       category: Category['outdoor']
     },
+    {
+      id: 7, 
+      name: 'Horror Movie Coasters', 
+      photo: '/../public/halloweenCoasters.jpg',
+      availableUnits: 1,
+      description: '',
+      category: Category['holiday']
+    },
+    {
+      id: 8, 
+      name: 'Jurassic Park Sign', 
+      photo: '/../public/jurassicPark.jpg',
+      availableUnits: 1,
+      description: '',
+      category: Category['decor']
+    },
+    {
+      id: 9, 
+      name: 'Frankenstein Sign', 
+      photo: '/../public/frankenstein.jpg',
+      availableUnits: 1,
+      description: '',
+      category: Category['decor']
+    },
+    {
+      id: 10, 
+      name: 'Dinosaur', 
+      photo: '/../public/dinosaur.jpg',
+      availableUnits: 1,
+      description: '',
+      category: Category['kids']
+    },
+    {
+      id: 11, 
+      name: 'Donald Duck Sign', 
+      photo: '/../public/donaldDuck.jpg',
+      availableUnits: 1,
+      description: '',
+      category: Category['kids']
+    },
+    {
+      id: 12, 
+      name: 'Compass Carving', 
+      photo: '/../public/compass.jpg',
+      availableUnits: 1,
+      description: '',
+      category: Category['decor']
+    },
+    {
+      id: 13, 
+      name: 'Bigfoot Sign', 
+      photo: '/../public/bigfoot.jpg',
+      availableUnits: 1,
+      description: '',
+      category: Category['decor']
+    },
+    {
+      id: 14, 
+      name: 'Headstones', 
+      photo: '/../public/headstones.jpg',
+      availableUnits: 1,
+      description: '',
+      category: Category['holiday']
+    },   
   ];
